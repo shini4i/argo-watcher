a1d1fd5bc2750065386c5035bd09d39752fb44a9
Vadim Gedz <vadims@linux-tech.io>
2022-03-17 18:16:19 +0200

    feat: add app refresh trigger (#3)

--- watcher/argo.py
+++ watcher/argo.py
@@ -3,6 +3,7 @@ import logging
 import json
 
 from tenacity import retry, stop_after_delay, retry_if_exception_type, wait_fixed
+from typing import Optional
 
 from watcher.settings import Settings
 from watcher.models import Images
@@ -16,19 +17,24 @@ class AppNotReadyException(Exception):
     pass
 
 
+class AppDoesNotExistException(Exception):
+    pass
+
+
 class Argo:
     def __init__(self):
         self.session = requests.session()
+        self.argo_url = Settings.Argo.url
+        self.argo_user = Settings.Argo.user
+        self.argo_password = Settings.Argo.password
         self.authorized = self.auth()
 
     def auth(self) -> bool:
-        status_code = self.session.post(url=f"{Settings.Argo.url}/api/v1/session",
-                                        json={
-                                            "username": Settings.Argo.user,
-                                            "password": Settings.Argo.password
-                                        }).status_code
-
-        match status_code:
+        match self.session.post(url=f"{self.argo_url}/api/v1/session",
+                                json={
+                                    "username": self.argo_user,
+                                    "password": self.argo_password
+                                }).status_code:
             case 200:
                 return True
             case 401:
@@ -38,18 +44,21 @@ class Argo:
                 logging.error("Forbidden, please check the firewall!")
                 return False
 
-    def get_app_status(self, app: str):
-        r = self.session.get(url=f"{Settings.Argo.url}/api/v1/applications/{app}")
+    def refresh_app(self, app: str) -> int:
+        return self.session.get(url=f"{self.argo_url}/api/v1/applications/{app}?refresh=normal").status_code
+
+    def get_app_status(self, app: str) -> Optional[dict]:
+        r = self.session.get(url=f"{self.argo_url}/api/v1/applications/{app}")
         if r.status_code != 200:
             return None
         else:
             app = json.loads(r.content.decode('utf-8'))
 
         status = {
-                    "images": app['status']['summary']['images'],
-                    'synced': app['status']['sync']['status'],
-                    'healthy': app['status']['health']['status']
-                  }
+            "images": app['status']['summary']['images'],
+            'synced': app['status']['sync']['status'],
+            'healthy': app['status']['health']['status']
+        }
 
         return status
 
@@ -57,6 +66,13 @@ class Argo:
            retry=retry_if_exception_type((AppNotReadyException, InvalidImageException)),
            wait=wait_fixed(5))
     def wait_for_rollout(self, payload: Images):
+
+        match self.refresh_app(app=payload.app):
+            case 200:
+                logging.info("test")
+            case 404:
+                raise AppDoesNotExistException
+
         app_status = self.get_app_status(payload.app)
         for target in payload.images:
             if f"{target.image}:{target.tag}" not in app_status['images']:
