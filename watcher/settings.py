29f510f81a8c83f017b0a04aca538ad28463a2cc
Vadim Gedz <vadims@linux-tech.io>
2022-03-20 16:19:24 +0200

    feat: make app fail if mandatory variables are not set (#15)

--- watcher/settings.py
+++ watcher/settings.py
@@ -1,22 +1,22 @@
 import logging
 
-from os import getenv
+from os import getenv, environ
 
 
 class Settings:
     class Argo:
-        url = getenv("ARGO_URL")
-        user = getenv("ARGO_USER")
-        password = getenv("ARGO_PASSWORD")
-        timeout = getenv("ARGO_TIMEOUT", 300)
+        url = environ['ARGO_URL']
+        user = environ['ARGO_USER']
+        password = environ['ARGO_PASSWORD']
+        timeout = getenv('ARGO_TIMEOUT', 300)
         if not isinstance(timeout, int):
             timeout = int(timeout)
 
     class Watcher:
-        history_ttl = getenv("HISTORY_TTL", 3600)
+        history_ttl = getenv('HISTORY_TTL', 3600)
         if not isinstance(history_ttl, int):
             history_ttl = int(history_ttl)
 
     class Logs:
-        log_level = logging.getLevelName(getenv("LOG_LEVEL", "DEBUG"))
-        json_logs = True if getenv("JSON_LOGS", "0") == "1" else False
+        log_level = logging.getLevelName(getenv('LOG_LEVEL', 'DEBUG'))
+        json_logs = True if getenv('JSON_LOGS', '0') == '1' else False
