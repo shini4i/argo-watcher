0072cb89c806b5d2871f61349b23e528526e9a86
Vadims Gedzs <vadims@linux-tech.io>
2022-03-20 16:17:10 +0200

    double quotes to single quotes

--- watcher/settings.py
+++ watcher/settings.py
@@ -8,15 +8,15 @@ class Settings:
         url = environ['ARGO_URL']
         user = environ['ARGO_USER']
         password = environ['ARGO_PASSWORD']
-        timeout = getenv("ARGO_TIMEOUT", 300)
+        timeout = getenv('ARGO_TIMEOUT', 300)
         if not isinstance(timeout, int):
             timeout = int(timeout)
 
     class Watcher:
-        history_ttl = getenv("HISTORY_TTL", 3600)
+        history_ttl = getenv('HISTORY_TTL', 3600)
         if not isinstance(history_ttl, int):
             history_ttl = int(history_ttl)
 
     class Logs:
-        log_level = logging.getLevelName(getenv("LOG_LEVEL", "DEBUG"))
-        json_logs = True if getenv("JSON_LOGS", "0") == "1" else False
+        log_level = logging.getLevelName(getenv('LOG_LEVEL', 'DEBUG'))
+        json_logs = True if getenv('JSON_LOGS', '0') == '1' else False
