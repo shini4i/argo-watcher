a09d11706d93a165d09f620c55d40bab67e22f3c
Bogdans Ozerkins <b.ozerkins@outlook.com>
2022-03-20 12:18:47 +0200

    re-organize component imports

--- web/src/App.js
+++ web/src/App.js
@@ -1,19 +1,18 @@
 import {useEffect, useState} from "react";
-
+import Navbar from "./Navbar";
+import ErrorSnackbar from "./ErrorSnackbar";
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import Navbar from "./Navbar";
 import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
+import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import RefreshIcon from '@mui/icons-material/Refresh';
-import ErrorSnackbar from "./ErrorSnackbar";
 
 function App() {
   const [tasks, setTasks] = useState([]);
