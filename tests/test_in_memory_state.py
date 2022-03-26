64b2b5c32b0e146ff8a20a116929bcbb91476f49
Vadims Gedzs <vadims@linux-tech.io>
2022-03-27 01:46:32 +0200

    minor changes

--- tests/test_in_memory_state.py
+++ tests/test_in_memory_state.py
@@ -1,28 +1,29 @@
 import pytest
+
 from uuid import uuid1
 from time import sleep
 
 from watcher.state import InMemoryState
 from watcher.models import Task
 
+task_template = {
+    "app": "test_app",
+    "author": "test_author",
+    "project": "test_project",
+    "images": [{"image": "test_image", "tag": "test_tag"}]
+}
+
 
 def test_task_status():
     state = InMemoryState()
 
-    task_id = str(uuid1())
-
-    task = {
-        "id": task_id,
-        "app": "test_app",
-        "author": "test_author",
-        "project": "test_project",
-        "images": [{"image": "test_image", "tag": "test_tag"}]
-    }
+    task = task_template
+    task['id'] = str(uuid1())
 
     state.set_current_task(task=Task(**task), status="in progress")
 
     try:
-        assert state.get_task_status(task_id=task_id) == "in progress"
+        assert state.get_task_status(task_id=task['id']) == "in progress"
         state.timer.cancel()
     except AssertionError:
         state.timer.cancel()
@@ -32,21 +33,14 @@ def test_task_status():
 def test_task_expiration():
     state = InMemoryState(retry_interval=1)
 
-    task_id = str(uuid1())
-
-    task = {
-        "id": task_id,
-        "app": "test_app",
-        "author": "test_author",
-        "project": "test_project",
-        "images": [{"image": "test_image", "tag": "test_tag"}]
-    }
+    task = task_template
+    task['id'] = str(uuid1())
 
     state.set_current_task(task=Task(**task), status="in progress")
-    sleep(5)
 
     try:
-        assert state.get_task_status(task_id=task_id) == "task not found"
+        sleep(5)
+        assert state.get_task_status(task_id=task['id']) == "task not found"
         state.timer.cancel()
     except AssertionError:
         state.timer.cancel()
