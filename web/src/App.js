2f5e8f44343362e7bb3991dc079e1513adfbfd26
Bogdans Ozerkins <b.ozerkins@outlook.com>
2022-03-28 21:58:10 +0300

    feat: add ui sorting and auto-refresh (#30)

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import Navbar from "./Navbar";
 import ErrorSnackbar from "./ErrorSnackbar";
 import {relativeTime} from "./Utils";
@@ -20,131 +20,231 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
+const timeframes = {
+  '5 minutes': 5 * 60,
+  '15 minutes': 15 * 60,
+  '30 minutes': 30 * 60,
+  '1 hour': 60 * 60
+};
+
+const autoRefreshIntervals = {
+  '5s': 5,
+  '10s': 10,
+  '30s': 30,
+  '1m': 60,
+  'off': 0,
+};
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [currentSort, setCurrentSort] = useState({field: "created", direction: "ASC"});
+  const [currentAutoRefresh, setCurrentAutoRefresh] = useState(autoRefreshIntervals['30s']);
+  const autoRefreshIntervalRef = useRef(null);
+
   const [loadingError, setLoadingError] = useState(null);
-  const timeframes = {
-    '5 minutes': 5 * 60,
-    '15 minutes': 15 * 60,
-    '30 minutes': 30 * 60,
-    '1 hour': 60 * 60
-  };
-  const [timeframe, setTimeframe] = useState(timeframes['5 minutes']);
+  const [currentTimeframe, setCurrentTimeframe] = useState(timeframes['5 minutes']);
 
   const refreshTasks = (timeframe) => {
-      let timestamp = Math.floor(Date.now() / 1000) - timeframe;
-
-      fetch(`/api/v1/tasks?timestamp=${timestamp}`)
-          .then(res => {
-              if (res.status !== 200) {
-                  throw new Error(res.statusText);
-              }
-              return res.json();
-          })
-          .then(items => {
-              setTasks(items);
-          })
-          .catch(error => {
-              setLoadingError(error.message);
-          })
-      ;
+    let timestamp = Math.floor(Date.now() / 1000) - timeframe;
+    fetch(`/api/v1/tasks?timestamp=${timestamp}`)
+        .then(res => {
+          if (res.status !== 200) {
+            throw new Error(res.statusText);
+          }
+          return res.json();
+        })
+        .then(items => {
+          setTasksSorted(items, currentSort);
+        })
+        .catch(error => {
+          setLoadingError(error.message);
+        })
+    ;
   };
 
+  const setTasksSorted = (tasks, sort) => {
+    // sort tasks
+    tasks.sort((a, b) => {
+      let aField = a[sort.field];
+      let bField = b[sort.field];
+      if (aField === bField) {
+        return 0;
+      }
+      if (aField > bField) {
+        return sort.direction === 'ASC' ? -1 : 1;
+      } else {
+        return sort.direction === 'ASC' ? 1 : -1;
+      }
+    });
+
+    // save sorted tasks
+    setTasks([].concat(tasks));
+  };
+
+  const triggerSortChange = (field) => {
+    // change sort parameters
+    let sortFieldChange = {...currentSort};
+    if (sortFieldChange.field === field) {
+      sortFieldChange.direction = sortFieldChange.direction === 'ASC' ? 'DESC' : 'ASC';
+    } else {
+      sortFieldChange.field = field;
+      sortFieldChange.direction = 'ASC';
+    }
+    setCurrentSort(sortFieldChange);
+    // set sorted tasks
+    setTasksSorted(tasks, sortFieldChange);
+  };
+
+  // initial load
   useEffect(() => {
-      refreshTasks(timeframe);
+    refreshTasks(currentTimeframe);
   }, []);
 
+  // we reset interval on any state change (because we use the state variables for data retrieval)
+  useEffect(() => {
+    // reset current interval
+    if (autoRefreshIntervalRef.current !== null) {
+      clearInterval(autoRefreshIntervalRef.current);
+    }
+    if (!currentAutoRefresh) { // value is 0 for "off"
+      return;
+    }
+    // set interval
+    autoRefreshIntervalRef.current = setInterval(() => {
+      refreshTasks(currentTimeframe);
+    }, currentAutoRefresh * 1000);
 
-  const handleChange = (event) => {
-    setTimeframe(event.target.value);
+    // clear interval on exit
+    return () => {
+      if (autoRefreshIntervalRef.current !== null) {
+        clearInterval(autoRefreshIntervalRef.current);
+      }
+    };
+  });
+
+  const handleTimeframeChange = (event) => {
+    setCurrentTimeframe(event.target.value);
     refreshTasks(event.target.value);
   };
 
+  const handleAutoRefreshChange = (event) => {
+    // save auto refresh interval
+    setCurrentAutoRefresh(event.target.value);
+  };
+
+  const TableCellSorted = ({field, children}) => {
+    return <TableCell
+      onClick={() => {  triggerSortChange(field); }}
+      sx={{cursor: "pointer",}}
+    >
+      <Box sx={{display: "flex", flexDirection: "row",}}>
+        {children} {currentSort.field === field && (currentSort.direction === "ASC" ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />)}
+      </Box>
+    </TableCell>
+  };
+
   return (
-    <div>
-        <Navbar />
+      <div>
+        <Navbar/>
         <Container maxWidth="xl">
-            <Stack direction="row" spacing={2} alignItems="center">
-                <Typography variant="h4" gutterBottom component="div" sx={{ flexGrow: 1 }}>
-                    Existing tasks
-                </Typography>
-                <IconButton edge="start" color="inherit" onClick={() => {
-                  refreshTasks(timeframe);
-                }}>
-                  <RefreshIcon />
-                </IconButton>
-                <Box sx={{ minWidth: 120 }}>
-                    <FormControl fullWidth size={"small"}>
-                        <InputLabel>Timeframe</InputLabel>
-                        <Select
-                            value={timeframe}
-                            label="Timeframe"
-                            onChange={handleChange}
-                        >
-                          {Object.keys(timeframes).map(timeframe => {
-                              let value = timeframes[timeframe];
-                              return <MenuItem key={timeframe} value={value}>{timeframe}</MenuItem>
-                          })}
-                        </Select>
-                    </FormControl>
-                </Box>
-            </Stack>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>ID</TableCell>
-                            <TableCell>Application</TableCell>
-                            <TableCell>Project</TableCell>
-                            <TableCell>Author</TableCell>
-                            <TableCell>Status</TableCell>
-                            <TableCell>Started</TableCell>
-                            <TableCell>Updated</TableCell>
-                            <TableCell>Images</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {tasks.map((task) => (
-                            <TableRow
-                                key={task.id}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {task.id}
-                                </TableCell>
-                                <TableCell>{task.app}</TableCell>
-                                <TableCell>{task.project}</TableCell>
-                                <TableCell>{task.author}</TableCell>
-                                <TableCell>{task.status}</TableCell>
-                                <TableCell>
-                                    <Tooltip title={new Date(task.created * 1000).toISOString()}>
-                                        <span>{relativeTime(task.created * 1000)}</span>
-                                    </Tooltip>
-                                </TableCell>
-                                <TableCell>
-                                    <Tooltip title={new Date(task.updated * 1000).toISOString()}>
-                                        <span>{relativeTime(task.updated * 1000)}</span>
-                                    </Tooltip>
-                                </TableCell>
-                                <TableCell>
-                                    {task.images.map((item, index) => {
-                                        return <div key={index}>{item.image}:{item.tag}</div>
-                                    })}
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                        {tasks.length === 0 && <TableRow>
-                            <TableCell colSpan={100} sx={{textAlign: "center"}}>
-                                No tasks were found within provided timeframe
-                            </TableCell>
-                        </TableRow>}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <ErrorSnackbar message={loadingError} setMessage={setLoadingError}/>
+          <Stack direction="row" spacing={2} alignItems="center">
+            <Typography variant="h4" gutterBottom component="div" sx={{flexGrow: 1}}>
+              Existing tasks
+            </Typography>
+            <IconButton edge="start" color="inherit" onClick={() => {
+              refreshTasks(currentTimeframe);
+            }}>
+              <RefreshIcon/>
+            </IconButton>
+            <Box sx={{minWidth: 120}}>
+              <FormControl fullWidth size={"small"}>
+                <InputLabel>Auto-Refresh</InputLabel>
+                <Select
+                    value={currentAutoRefresh}
+                    label="Auto-Refresh"
+                    onChange={handleAutoRefreshChange}
+                >
+                  {Object.keys(autoRefreshIntervals).map(autoRefreshInterval => {
+                    let value = autoRefreshIntervals[autoRefreshInterval];
+                    return <MenuItem key={autoRefreshInterval} value={value}>{autoRefreshInterval}</MenuItem>
+                  })}
+                </Select>
+              </FormControl>
+            </Box>
+            <Box sx={{minWidth: 120}}>
+              <FormControl fullWidth size={"small"}>
+                <InputLabel>Timeframe</InputLabel>
+                <Select
+                    value={currentTimeframe}
+                    label="Timeframe"
+                    onChange={handleTimeframeChange}
+                >
+                  {Object.keys(timeframes).map(timeframe => {
+                    let value = timeframes[timeframe];
+                    return <MenuItem key={timeframe} value={value}>{timeframe}</MenuItem>
+                  })}
+                </Select>
+              </FormControl>
+            </Box>
+          </Stack>
+          <TableContainer component={Paper}>
+            <Table sx={{minWidth: 650}} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCellSorted field={"id"}>ID</TableCellSorted>
+                  <TableCellSorted field={"app"}>Application</TableCellSorted>
+                  <TableCellSorted field={"project"}>Project</TableCellSorted>
+                  <TableCellSorted field={"author"}>Author</TableCellSorted>
+                  <TableCellSorted field={"status"}>Status</TableCellSorted>
+                  <TableCellSorted field={"created"}>Started</TableCellSorted>
+                  <TableCellSorted field={"updated"}>Updated</TableCellSorted>
+                  <TableCellSorted field={"images"}>Images</TableCellSorted>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {tasks.map((task) => (
+                    <TableRow
+                        key={task.id}
+                        sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                    >
+                      <TableCell component="th" scope="row">
+                        {task.id}
+                      </TableCell>
+                      <TableCell>{task.app}</TableCell>
+                      <TableCell>{task.project}</TableCell>
+                      <TableCell>{task.author}</TableCell>
+                      <TableCell>{task.status}</TableCell>
+                      <TableCell>
+                        <Tooltip title={new Date(task.created * 1000).toISOString()}>
+                          <span>{relativeTime(task.created * 1000)}</span>
+                        </Tooltip>
+                      </TableCell>
+                      <TableCell>
+                        <Tooltip title={new Date(task.updated * 1000).toISOString()}>
+                          <span>{relativeTime(task.updated * 1000)}</span>
+                        </Tooltip>
+                      </TableCell>
+                      <TableCell>
+                        {task.images.map((item, index) => {
+                          return <div key={index}>{item.image}:{item.tag}</div>
+                        })}
+                      </TableCell>
+                    </TableRow>
+                ))}
+                {tasks.length === 0 && <TableRow>
+                  <TableCell colSpan={100} sx={{textAlign: "center"}}>
+                    No tasks were found within provided timeframe
+                  </TableCell>
+                </TableRow>}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <ErrorSnackbar message={loadingError} setMessage={setLoadingError}/>
         </Container>
-    </div>
+      </div>
   );
 }
 
