67d2734e22e73fd93d52fdcd3222d8c64751e7b6
Vadims Gedzs <vadims@linux-tech.io>
2022-04-20 20:09:14 +0300

    minor changes

--- watcher/config.py
+++ watcher/config.py
@@ -1,6 +1,7 @@
 import logging
 
 from environs import Env
+from marshmallow.validate import OneOf
 
 env = Env()
 
@@ -13,15 +14,23 @@ class Config:
         timeout = env.int("ARGO_TIMEOUT", 300)
 
     class Watcher:
-        state_type = env.str("STATE_TYPE", "in-memory")
+        state_type = env.str(
+            "STATE_TYPE",
+            "in-memory",
+            validate=OneOf(
+                ["in-memory", "postgres"], error="STATE_TYPE must be one of {choices}"
+            ),
+        )
         ssl_verify = env.bool("SSL_VERIFY", True)
         history_ttl = env.int("HISTORY_TTL", 3600)
 
-    class DB:
-        host = env.str("DB_HOST")
-        db_name = env.str("DB_NAME")
-        db_user = env.str("DB_USER")
-        db_password = env.str("DB_PASSWORD")
+    if Watcher.state_type == "postgres":
+
+        class DB:
+            host = env.str("DB_HOST")
+            db_name = env.str("DB_NAME")
+            db_user = env.str("DB_USER")
+            db_password = env.str("DB_PASSWORD")
 
     class Logs:
         log_level = env.log_level("LOG_LEVEL", logging.INFO)
