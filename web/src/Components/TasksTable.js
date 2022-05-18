2ca80599a5e012e0ff2fd03876190a1309d3c92d
Bogdans Ozerkins <b.ozerkins@outlook.com>
2022-05-18 19:12:10 +0300

    improve table to contain more useful information

--- web/src/Components/TasksTable.js
+++ web/src/Components/TasksTable.js
@@ -5,7 +5,7 @@ import TableRow from "@mui/material/TableRow";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import Tooltip from "@mui/material/Tooltip";
-import {relativeTime, relativeTimestamp} from "../Utils";
+import {relativeHumanDuration, relativeTime, relativeTimestamp} from "../Utils";
 import TableContainer from "@mui/material/TableContainer";
 import React, {useEffect, useState} from "react";
 import {fetchTasks} from "../Services/Data";
@@ -26,6 +26,14 @@ const chipColorByStatus = (status) => {
   }
   return undefined;
 };
+
+const taskDuration = (created, updated) => {
+  if (!updated) {
+    updated = Math.round(Date.now()/1000);
+  }
+  const seconds = updated - created;
+  return relativeHumanDuration(seconds);
+}
 
 export function useTasks({ setLoadingError }) {
   const [tasks, setTasks] = useState([]);
@@ -109,13 +117,12 @@ function TasksTable({ tasks, sortField, setSortField }) {
         <Table sx={{minWidth: 650}} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCellSorted sortField={sortField} setSortField={setSortField} field={"id"}>ID</TableCellSorted>
               <TableCellSorted sortField={sortField} setSortField={setSortField} field={"app"}>Application</TableCellSorted>
               <TableCellSorted sortField={sortField} setSortField={setSortField} field={"project"}>Project</TableCellSorted>
               <TableCellSorted sortField={sortField} setSortField={setSortField} field={"author"}>Author</TableCellSorted>
               <TableCellSorted sortField={sortField} setSortField={setSortField} field={"status"}>Status</TableCellSorted>
               <TableCellSorted sortField={sortField} setSortField={setSortField} field={"created"}>Started</TableCellSorted>
-              <TableCellSorted sortField={sortField} setSortField={setSortField} field={"updated"}>Updated</TableCellSorted>
+              <TableCellSorted sortField={sortField} setSortField={setSortField} field={"updated"}>Duration</TableCellSorted>
               <TableCellSorted sortField={sortField} setSortField={setSortField} field={"images"}>Images</TableCellSorted>
             </TableRow>
           </TableHead>
@@ -125,9 +132,6 @@ function TasksTable({ tasks, sortField, setSortField }) {
                     key={task.id}
                     sx={{'&:last-child td, &:last-child th': {border: 0}}}
                 >
-                  <TableCell component="th" scope="row">
-                    {task.id}
-                  </TableCell>
                   <TableCell>{task.app}</TableCell>
                   <TableCell>{task.project}</TableCell>
                   <TableCell>{task.author}</TableCell>
@@ -140,9 +144,14 @@ function TasksTable({ tasks, sortField, setSortField }) {
                     </Tooltip>
                   </TableCell>
                   <TableCell>
-                    <Tooltip title={new Date(task.updated * 1000).toISOString()}>
-                      <span>{relativeTime(task.updated * 1000)}</span>
-                    </Tooltip>
+                    {task.updated && (
+                      <Tooltip title={new Date(task.updated * 1000).toISOString()}>
+                        <span>{taskDuration(task.created, task.updated)}</span>
+                      </Tooltip>
+                    )}
+                    {!task.updated && (
+                        <span>-</span>
+                    )}
                   </TableCell>
                   <TableCell>
                     {task.images.map((item, index) => {
