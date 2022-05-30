89ab4505f38193abe333fbfe54682a148b2f088d
Vadims Gedzs <vadims@linux-tech.io>
2022-05-30 23:37:18 +0300

    update

--- client/client.go
+++ client/client.go
@@ -6,8 +6,10 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 type Image struct {
@@ -68,9 +70,45 @@ func (task *Task) send() string {
 	return accepted.Id
 }
 
+func (task *Task) getStatus(id string) string {
+	request, err := http.NewRequest("GET", os.Getenv("ARGO_WATCHER_URL")+"/api/v1/tasks/"+id, nil)
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(response.Body)
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	type TaskStatus struct {
+		Status string `json:"status"`
+	}
+
+	var accepted TaskStatus
+	err = json.Unmarshal(body, &accepted)
+	if err != nil {
+		panic(err)
+	}
+
+	return accepted.Status
+}
+
 func main() {
 	task := Task{
-		App:     "test",
+		App:     "whoami",
 		Author:  "John",
 		Project: "whoami",
 		Images: []Image{
@@ -80,5 +118,25 @@ func main() {
 			},
 		},
 	}
-	fmt.Println(task.send())
+	id := task.send()
+
+	time.Sleep(5 * time.Second)
+
+loop:
+	for {
+		switch status := task.getStatus(id); status {
+		case "failed":
+			fmt.Println("The deployment has failed, please check logs.")
+			break loop
+		case "in progress":
+			fmt.Println("Application deployment is in progress..")
+			time.Sleep(5 * time.Second)
+		case "app not found":
+			fmt.Println("Application " + task.App + " does not exist")
+			break loop
+		case "deployed":
+			fmt.Println("done")
+			break loop
+		}
+	}
 }
