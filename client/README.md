ca3eca94bd8728d46b91ecd15bec1218f1f9767e
Vadim Gedz <vadims@linux-tech.io>
2022-05-31 15:38:41 +0300

    chore(client): python -> go (#70)

--- client/README.md
+++ client/README.md
@@ -1,14 +1,31 @@
-# client
+# Client
 
 This is just an example of what can be used within pipeline to communicate with argo-watcher
 
-## Mandatory environment variable
+## Environment variable
 
-| Variable | Description |
-|---|---|
-| ARGO_WATCHER_URL | The url of argo-watcher instance |
-| ARGO_APP | The name of argo app to check for images rollout |
-| COMMIT_AUTHOR | The person who made commit/triggered pipeline |
-| PROJECT_NAME | An identificator of the business project (not related to argo project) |
-| IMAGES | A list of images (separated by ",") that should contain specific tag |
-| IMAGE_TAG | An image tag that is expected to be rolled out |
+| Variable | Description | Mandatory |
+|---|---|---|
+| ARGO_WATCHER_URL | The url of argo-watcher instance | Yes
+| ARGO_APP | The name of argo app to check for images rollout | Yes
+| COMMIT_AUTHOR | The person who made commit/triggered pipeline | Yes
+| PROJECT_NAME | An identificator of the business project (not related to argo project) | Yes
+| IMAGES | A list of images (separated by ",") that should contain specific tag | Yes
+| IMAGE_TAG | An image tag that is expected to be rolled out | Yes
+| DEBUG | Print various debug information | No
+
+## Example configuration
+### gitlab-ci
+```yaml
+await-deployment:
+  image: ghcr.io/shini4i/argo-watcher-client:v0.0.7
+  variables:
+    ARGO_WATCHER_URL: argo-watcher.example.com
+    ARGO_APP: example
+    COMMIT_AUTHOR: $GITLAB_USER_EMAIL
+    PROJECT_NAME: $CI_PROJECT_PATH
+    IMAGES: $CI_REGISTRY_IMAGE
+    IMAGE_TAG: $CI_PIPELINE_ID
+    DEBUG: True
+  script: ["/bin/client"]
+```
---
+++ client/client.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Image struct {
+	Image string `json:"image"`
+	Tag   string `json:"tag"`
+}
+
+type Task struct {
+	App     string  `json:"app"`
+	Author  string  `json:"author"`
+	Project string  `json:"project"`
+	Images  []Image `json:"images"`
+}
+
+func (task *Task) send() string {
+	body, err := json.Marshal(task)
+	if err != nil {
+		panic(err)
+	}
+
+	request, err := http.NewRequest("POST", os.Getenv("ARGO_WATCHER_URL")+"/api/v1/tasks", bytes.NewBuffer(body))
+	if err != nil {
+		panic(err)
+	}
+
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(response.Body)
+
+	body, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	if response.StatusCode != 202 {
+		fmt.Printf("Something went wrong on argo-watcher side. Got the following response code %d\n", response.StatusCode)
+		fmt.Printf("Body: %s\n", string(body))
+		os.Exit(1)
+	}
+
+	type AcceptedTask struct {
+		Status string `json:"status"`
+		Id     string `json:"id"`
+	}
+
+	var accepted AcceptedTask
+	err = json.Unmarshal(body, &accepted)
+	if err != nil {
+		panic(err)
+	}
+
+	return accepted.Id
+}
+
+func (task *Task) getStatus(id string) string {
+	request, err := http.NewRequest("GET", os.Getenv("ARGO_WATCHER_URL")+"/api/v1/tasks/"+id, nil)
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(response.Body)
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if response.StatusCode != 200 {
+		fmt.Printf("Received non 200 status code (%d)\n", response.StatusCode)
+		fmt.Printf("Body: %s\n", string(body))
+		os.Exit(1)
+	}
+
+	type TaskStatus struct {
+		Status string `json:"status"`
+	}
+
+	var accepted TaskStatus
+	err = json.Unmarshal(body, &accepted)
+	if err != nil {
+		panic(err)
+	}
+
+	return accepted.Status
+}
+
+func main() {
+	tag := os.Getenv("IMAGE_TAG")
+	var images []Image
+
+	for _, image := range strings.Split(os.Getenv("IMAGES"), ",") {
+		images = append(images, Image{
+			image,
+			tag,
+		})
+	}
+
+	task := Task{
+		App:     os.Getenv("ARGO_APP"),
+		Author:  os.Getenv("COMMIT_AUTHOR"),
+		Project: os.Getenv("PROJECT_NAME"),
+		Images:  images,
+	}
+
+	fmt.Printf("Waiting for %s app to be running on %s version.\n", task.App, tag)
+
+	debug, _ := strconv.ParseBool(os.Getenv("DEBUG"))
+
+	if debug {
+		fmt.Printf("Got the following configuration:\n"+
+			"ARGO_WATCHER_URL: %s\nARGO_APP: %s\n"+
+			"COMMIT_AUTHOR: %s\n"+
+			"PROJECT_NAME: %s\n"+
+			"IMAGE_TAG: %s\n"+
+			"IMAGES: %s\n",
+			os.Getenv("ARGO_WATCHER_URL"), task.App, task.Author, task.Project, tag, os.Getenv("IMAGES"))
+	}
+
+	id := task.send()
+
+	time.Sleep(5 * time.Second)
+
+loop:
+	for {
+		switch status := task.getStatus(id); status {
+		case "failed":
+			fmt.Println("The deployment has failed, please check logs.")
+			break loop
+		case "in progress":
+			fmt.Println("Application deployment is in progress...")
+			time.Sleep(15 * time.Second)
+		case "app not found":
+			fmt.Printf("Application %s does not exist.\n", task.App)
+			break loop
+		case "deployed":
+			fmt.Printf("The deployment of %s version is done.\n", tag)
+			break loop
+		}
+	}
+}
