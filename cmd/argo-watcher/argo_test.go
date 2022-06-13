a1469f84d5a6d4e65eb3e6b806a1c861ebab4db9
Vadim Gedz <vadims@linux-tech.io>
2022-06-13 20:43:14 +0300

    chore: add more tests and mock service (#80)

---
+++ cmd/argo-watcher/argo_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"github.com/shini4i/argo-watcher/internal/helpers"
+	m "github.com/shini4i/argo-watcher/internal/models"
+	"testing"
+	"time"
+)
+
+var taskId string
+
+var (
+	testArgo = Argo{
+		Url:      "http://localhost:8081",
+		User:     "watcher",
+		Password: "test",
+	}
+	testClient = testArgo.Init()
+	task       = m.Task{
+		Created: float64(time.Now().Unix()),
+		App:     "Test",
+		Author:  "Test Author",
+		Project: "Test Project",
+		Images: []m.Image{
+			{
+				Image: "test",
+				Tag:   "v0.0.1",
+			},
+		},
+		Status: "in progress",
+	}
+)
+
+func TestArgo_GetTaskStatus(t *testing.T) {
+	taskId = testClient.AddTask(task)
+
+	time.Sleep(5 * time.Second)
+
+	if status := testClient.GetTaskStatus(taskId); status != "deployed" {
+		t.Errorf("got %s, expected %s", status, "in progress")
+	}
+}
+
+func TestArgo_GetAppList(t *testing.T) {
+	apps := testClient.GetAppList()
+
+	for _, app := range apps {
+		if !helpers.Contains([]string{"Test", "Test2"}, app) {
+			t.Errorf("Got unexpected value %s", app)
+		}
+	}
+
+	if len(apps) != 1 {
+		t.Errorf("Got %d apps, but expected %d", len(apps), 2)
+	}
+}
