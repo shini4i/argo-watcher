fd5a959aa0cc0898ce90a5ddfbc9509860c083dc
Bogdans Ozerkins <b.ozerkins@outlook.com>
2022-06-13 17:10:59 +0300

    feat: add version display to navigation

--- web/src/Components/Navbar.js
+++ web/src/Components/Navbar.js
@@ -7,6 +7,9 @@ import Link from "@mui/material/Link";
 import Button from "@mui/material/Button";
 import { Link as RouterLink } from "react-router-dom";
 import Stack from "@mui/material/Stack";
+import {fetchApplications, fetchVersion} from "../Services/Data";
+import {useEffect, useState} from "react";
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
 
 function NavigationButton({to, children}) {
     return <Link
@@ -19,6 +22,15 @@ function NavigationButton({to, children}) {
 }
 
 function Navbar() {
+
+    const [version, setVersion] = useState('0.0.0');
+
+    useEffect(() => {
+        fetchVersion()
+            .then(version => { setVersion(version); })
+            .catch(error => { console.log(error.message); });
+    }, []);
+
     return <Box sx={{ mb: 2}}>
         <AppBar position="static">
             <Toolbar>
@@ -29,13 +41,26 @@ function Navbar() {
                     <NavigationButton to={"/"}>Recent</NavigationButton>
                     <NavigationButton to={"/history"}>History</NavigationButton>
                 </Stack>
-                <Button endIcon={<GitHubIcon />}
-                        sx={{ color: 'white', textTransform: 'unset'}}
-                        size={"small"}
-                        component={Link}
-                        href={"https://github.com/shini4i/argo-watcher"}>
-                    View on GitHub
-                </Button>
+                <Stack spacing={1} direction={"row"} alignItems={"center"}
+                       sx={{
+                           color: 'white', textTransform: 'unset', textDecoration: "unset",
+                           '&:hover': {
+                               color: 'rgba(255,255,255,.85)'
+                           }
+                        }}
+                       component={Link}
+                       href={"https://github.com/shini4i/argo-watcher/tree/v" + version}
+                >
+                    <GitHubIcon sx={{ fontSize: "1.7em" }} />
+                    <Stack>
+                        <Typography fontSize={"14px"}>
+                            GitHub
+                        </Typography>
+                        <Typography fontSize={"11px"}>
+                            <LocalOfferIcon fontSize={"6px"}/> {version}
+                        </Typography>
+                    </Stack>
+                </Stack>
             </Toolbar>
         </AppBar>
     </Box>
