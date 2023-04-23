4480ef6f48d293d4cd0ff3d4f205647a14685179
Bogdans Ozerkins <b.ozerkins@outlook.com>
2023-04-23 14:25:41 -0700

    indent with spaces

--- internal/models/constants.go
+++ internal/models/constants.go
@@ -1,9 +1,9 @@
 package models
 
-const StatusAppNotFoundMessage =       		"app not found";
-const StatusInProgressMessage =        		"in progress";
-const StatusFailedMessage =            		"failed";
-const StatusArgoCDUnavailableMessage = 		"argocd is unavailable";
-const StatusConnectionUnavailable = 		"cannot connect to database";
-const StatusArgoCDFailedLogin =		 		"failed to login to argocd";
-const StatusDeployedMessage =          		"deployed";
+const StatusAppNotFoundMessage =            "app not found";
+const StatusInProgressMessage =             "in progress";
+const StatusFailedMessage =                 "failed";
+const StatusArgoCDUnavailableMessage =      "argocd is unavailable";
+const StatusConnectionUnavailable =         "cannot connect to database";
+const StatusArgoCDFailedLogin =             "failed to login to argocd";
+const StatusDeployedMessage =               "deployed";
