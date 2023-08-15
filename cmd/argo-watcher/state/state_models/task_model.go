a71fd2cdb73fff53c91ea04a4c85f3f0529e994a
Bogdans Ozerkins <b.ozerkins@outlook.com>
2023-08-15 10:07:35 -0700

    fix: model migration not null breaks upgrade (#170)

--- cmd/argo-watcher/state/state_models/task_model.go
+++ cmd/argo-watcher/state/state_models/task_model.go
@@ -12,13 +12,13 @@ import (
 type TaskModel struct {
 	Id              uuid.UUID                         `gorm:"column:id;type:uuid;default:gen_random_uuid()"`
 	Created         time.Time                         `gorm:"column:created;autoCreateTime;not null;index;"`
-	Updated         time.Time                         `gorm:"column:updated;autoUpdateTime;not null;"`
+	Updated         time.Time                         `gorm:"column:updated;autoUpdateTime;"`
 	Images          datatypes.JSONSlice[models.Image] `gorm:"column:images;not null;"`
 	Status          string                            `gorm:"column:status;type:VARCHAR(20);not null;index;"`
-	ApplicationName sql.NullString                    `gorm:"column:app;type:VARCHAR(255);not null;"`
-	Author          sql.NullString                    `gorm:"column:author;type:VARCHAR(255);not null;"`
-	Project         sql.NullString                    `gorm:"column:project;type:VARCHAR(255);not null;"`
-	StatusReason    sql.NullString                    `gorm:"column:status_reason;default:''"`
+	ApplicationName sql.NullString                    `gorm:"column:app;type:VARCHAR(255);"`
+	Author          sql.NullString                    `gorm:"column:author;type:VARCHAR(255);"`
+	Project         sql.NullString                    `gorm:"column:project;type:VARCHAR(255);"`
+	StatusReason    sql.NullString                    `gorm:"column:status_reason;"`
 }
 
 func (TaskModel) TableName() string {
