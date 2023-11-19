15a34672ac32b09530b3f8e1b890ef74c164bfab
Vadim Gedz <vadims@linux-tech.io>
2023-11-19 15:02:30 +0200

    chore: add TestNewClientConfig

---
+++ pkg/client/config_test.go
@@ -0,0 +1,37 @@
+package client
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewClientConfig(t *testing.T) {
+	t.Setenv("ARGO_WATCHER_URL", "http://localhost:8080")
+	t.Setenv("IMAGES", "image1,image2")
+	t.Setenv("IMAGE_TAG", "v1.0.0")
+	t.Setenv("ARGO_APP", "test-app")
+	t.Setenv("COMMIT_AUTHOR", "John Doe")
+	t.Setenv("PROJECT_NAME", "test-project")
+	t.Setenv("ARGO_WATCHER_DEPLOY_TOKEN", "test-token")
+	t.Setenv("TIMEOUT", "60s")
+	t.Setenv("DEBUG", "true")
+
+	// Call the function
+	config, err := NewClientConfig()
+
+	// Assert there was no error
+	assert.NoError(t, err)
+
+	// Assert the config was correctly populated
+	assert.Equal(t, "http://localhost:8080", config.Url)
+	assert.Equal(t, []string{"image1", "image2"}, config.Images)
+	assert.Equal(t, "v1.0.0", config.Tag)
+	assert.Equal(t, "test-app", config.App)
+	assert.Equal(t, "John Doe", config.Author)
+	assert.Equal(t, "test-project", config.Project)
+	assert.Equal(t, "test-token", config.Token)
+	assert.Equal(t, 60*time.Second, config.Timeout)
+	assert.Equal(t, true, config.Debug)
+}
