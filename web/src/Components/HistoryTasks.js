063a39ac8c5ea54f586f182cf2ed791dfc8466f8
Vadim Gedz <vadims@linux-tech.io>
2023-12-04 22:55:26 +0200

    chore: add footer with copyrigh

--- web/src/Components/HistoryTasks.js
+++ web/src/Components/HistoryTasks.js
@@ -14,151 +14,168 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { useErrorContext } from '../ErrorContext';
 
+const autoRefreshIntervals = {
+    '5s': 5,
+    '10s': 10,
+    '30s': 30,
+    '1m': 60,
+    off: 0,
+};
+
 function HistoryTasks() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const { setError, setSuccess } = useErrorContext();
-  const { tasks, sortField, setSortField, refreshTasksInRange, clearTasks } =
-    useTasks({ setError, setSuccess });
-  const [currentApplication, setCurrentApplication] = useState(
-    searchParams.get('app') ?? null,
-  );
-  const [dateRange, setDateRange] = useState([
-    Number(searchParams.get('start'))
-      ? new Date(Number(searchParams.get('start')) * 1000)
-      : startOfDay(new Date()),
-    Number(searchParams.get('end'))
-      ? new Date(Number(searchParams.get('end')) * 1000)
-      : startOfDay(new Date()),
-  ]);
-  const [startDate, endDate] = dateRange;
-  const [currentPage, setCurrentPage] = useState(
-    searchParams.get('page') ? Number(searchParams.get('page')) : 1,
-  );
+    const [searchParams, setSearchParams] = useSearchParams();
+    const { setError, setSuccess } = useErrorContext();
+    const { tasks, sortField, setSortField, refreshTasksInRange, clearTasks } =
+        useTasks({ setError, setSuccess });
+    const [currentApplication, setCurrentApplication] = useState(
+        searchParams.get('app') ?? null,
+    );
+    const [dateRange, setDateRange] = useState([
+        Number(searchParams.get('start'))
+            ? new Date(Number(searchParams.get('start')) * 1000)
+            : startOfDay(new Date()),
+        Number(searchParams.get('end'))
+            ? new Date(Number(searchParams.get('end')) * 1000)
+            : startOfDay(new Date()),
+    ]);
+    const [startDate, endDate] = dateRange;
+    const [currentPage, setCurrentPage] = useState(
+        searchParams.get('page') ? Number(searchParams.get('page')) : 1,
+    );
 
-  const updateSearchParameters = (start, end, application, page) => {
-    setSearchParams({
-      app: application ?? '',
-      start: Math.floor(start.getTime() / 1000),
-      end: Math.floor(end.getTime() / 1000),
-      page,
-    });
-  };
+    const updateSearchParameters = (start, end, application, page) => {
+        setSearchParams({
+            app: application ?? '',
+            start: Math.floor(start.getTime() / 1000),
+            end: Math.floor(end.getTime() / 1000),
+            page,
+        });
+    };
 
-  const refreshWithFilters = (start, end, application, page) => {
-    if (start && end) {
-      refreshTasksInRange(
-        Math.floor(startOfDay(start).getTime() / 1000),
-        Math.floor(endOfDay(end).getTime() / 1000),
-        application,
-      );
-      updateSearchParameters(start, end, application, page);
-    } else {
-      clearTasks();
-    }
-  };
-
-  useEffect(() => {
-    refreshWithFilters(startDate, endDate, currentApplication, currentPage);
-  }, []);
-
-  const DateRangePickerCustomInput = forwardRef(({ value, onClick }, ref) => (
-    <TextField
-      size="small"
-      sx={{ minWidth: '220px' }}
-      onClick={onClick}
-      ref={ref}
-      value={value}
-      label={'Date range'}
-    />
-  ));
-
-  return (
-    <Container maxWidth="xl">
-      <Stack
-        direction={{ xs: 'column', md: 'row' }}
-        spacing={2}
-        alignItems="center"
-        sx={{ mb: 2 }}
-      >
-        <Typography
-          variant="h5"
-          gutterBottom
-          component="div"
-          sx={{
-            flexGrow: 1,
-            display: 'flex',
-            gap: '10px',
-            m: 0,
-            alignItems: 'center',
-          }}
-        >
-          <Box>History tasks</Box>
-          <Box sx={{ fontSize: '10px' }}>UTC</Box>
-        </Typography>
-        <Stack direction="row" spacing={2}>
-          <Box>
-            <ApplicationsFilter
-              value={currentApplication}
-              onChange={value => {
-                setCurrentApplication(value);
-                setCurrentPage(1);
-                refreshWithFilters(startDate, endDate, value, 1);
-              }}
-              setError={setError}
-              setSuccess={setSuccess}
-            />
-          </Box>
-          <Box>
-            <DatePicker
-              selectsRange={true}
-              startDate={startDate}
-              endDate={endDate}
-              onChange={update => {
-                setDateRange(update);
-                setCurrentPage(1);
-                refreshWithFilters(update[0], update[1], currentApplication, 1);
-              }}
-              maxDate={new Date()}
-              isClearable={false}
-              customInput={<DateRangePickerCustomInput />}
-              required
-            />
-          </Box>
-          <Box>
-            <IconButton
-              edge="start"
-              color={'primary'}
-              title={'Reload table'}
-              onClick={() => {
-                setCurrentPage(1);
-                refreshWithFilters(startDate, endDate, currentApplication, 1);
-              }}
-            >
-              <RefreshIcon />
-            </IconButton>
-          </Box>
-        </Stack>
-      </Stack>
-      <Box sx={{ boxShadow: 2, borderRadius: 2, p: 2 }}>
-        <TasksTable
-          tasks={tasks}
-          sortField={sortField}
-          setSortField={setSortField}
-          relativeDate={false}
-          page={currentPage}
-          onPageChange={page => {
-            setCurrentPage(page);
-            updateSearchParameters(
-              startDate,
-              endDate,
-              currentApplication,
-              page,
+    const refreshWithFilters = (start, end, application, page) => {
+        if (start && end) {
+            refreshTasksInRange(
+                Math.floor(startOfDay(start).getTime() / 1000),
+                Math.floor(endOfDay(end).getTime() / 1000),
+                application,
             );
-          }}
+            updateSearchParameters(start, end, application, page);
+        } else {
+            clearTasks();
+        }
+    };
+
+    useEffect(() => {
+        refreshWithFilters(startDate, endDate, currentApplication, currentPage);
+    }, []);
+
+    const DateRangePickerCustomInput = forwardRef(({ value, onClick }, ref) => (
+        <TextField
+            size="small"
+            sx={{ minWidth: '220px' }}
+            onClick={onClick}
+            ref={ref}
+            value={value}
+            label={'Date range'}
         />
-      </Box>
-    </Container>
-  );
+    ));
+
+    return (
+        <Container maxWidth="xl">
+            <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: 'calc(100vh - 40px)' }}>
+                <Box sx={{ flexGrow: 1 }}>
+                    <Stack
+                        direction={{ xs: 'column', md: 'row' }}
+                        spacing={2}
+                        alignItems="center"
+                        sx={{ mb: 2 }}
+                    >
+                        <Typography
+                            variant="h5"
+                            gutterBottom
+                            component="div"
+                            sx={{
+                                flexGrow: 1,
+                                display: 'flex',
+                                gap: '10px',
+                                m: 0,
+                                alignItems: 'center', // Center text vertically
+                            }}
+                        >
+                            <Box>History tasks</Box>
+                            <Box sx={{ fontSize: '10px' }}>UTC</Box>
+                        </Typography>
+                        <Stack direction="row" spacing={2}>
+                            <Box>
+                                <ApplicationsFilter
+                                    value={currentApplication}
+                                    onChange={value => {
+                                        setCurrentApplication(value);
+                                        setCurrentPage(1);
+                                        refreshWithFilters(startDate, endDate, value, 1);
+                                    }}
+                                    setError={setError}
+                                    setSuccess={setSuccess}
+                                />
+                            </Box>
+                            <Box>
+                                <DatePicker
+                                    selectsRange={true}
+                                    startDate={startDate}
+                                    endDate={endDate}
+                                    onChange={update => {
+                                        setDateRange(update);
+                                        setCurrentPage(1);
+                                        refreshWithFilters(update[0], update[1], currentApplication, 1);
+                                    }}
+                                    maxDate={new Date()}
+                                    isClearable={false}
+                                    customInput={<DateRangePickerCustomInput />}
+                                    required
+                                />
+                            </Box>
+                            <Box>
+                                <IconButton
+                                    edge="start"
+                                    color={'primary'}
+                                    title={'Reload table'}
+                                    onClick={() => {
+                                        setCurrentPage(1);
+                                        refreshWithFilters(startDate, endDate, currentApplication, 1);
+                                    }}
+                                >
+                                    <RefreshIcon />
+                                </IconButton>
+                            </Box>
+                        </Stack>
+                    </Stack>
+                    <Box sx={{ boxShadow: 2, borderRadius: 2, p: 2 }}>
+                        <TasksTable
+                            tasks={tasks}
+                            sortField={sortField}
+                            setSortField={setSortField}
+                            relativeDate={false}
+                            page={currentPage}
+                            onPageChange={page => {
+                                setCurrentPage(page);
+                                updateSearchParameters(
+                                    startDate,
+                                    endDate,
+                                    currentApplication,
+                                    page,
+                                );
+                            }}
+                        />
+                    </Box>
+                </Box>
+            </Box>
+            <Box sx={{ position: 'fixed', bottom: 0, width: '100%', textAlign: 'center', padding: '10px' }}>
+                <Typography variant="body2" color="text.secondary">
+                    © {new Date().getFullYear()} Vadim Gedz. This project is licensed under the MIT License.
+                </Typography>
+            </Box>
+        </Container>
+    );
 }
 
 export default HistoryTasks;
--- web/src/Components/RecentTasks.js
+++ web/src/Components/RecentTasks.js
@@ -15,179 +15,188 @@ import TasksTable, { useTasks } from './TasksTable';
 import { useErrorContext } from '../ErrorContext';
 
 const autoRefreshIntervals = {
-  '5s': 5,
-  '10s': 10,
-  '30s': 30,
-  '1m': 60,
-  off: 0,
+    '5s': 5,
+    '10s': 10,
+    '30s': 30,
+    '1m': 60,
+    off: 0,
 };
 
 function RecentTasks() {
-  const { setError, setSuccess } = useErrorContext();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const { tasks, sortField, setSortField, refreshTasksInTimeframe } = useTasks({
-    setError,
-    setSuccess,
-  });
-
-  const [currentAutoRefresh, setCurrentAutoRefresh] = useState(
-    searchParams.get('refresh') ?? autoRefreshIntervals['30s'],
-  );
-  const autoRefreshIntervalRef = useRef(null);
-  const [currentApplication, setCurrentApplication] = useState(
-    searchParams.get('app') ?? null,
-  );
-  const currentTimeframe = 9 * 60 * 60;
-  const [currentPage, setCurrentPage] = useState(
-    searchParams.get('page') ? Number(searchParams.get('page')) : 1,
-  );
-
-  const updateSearchParameters = (application, refresh, page) => {
-    setSearchParams({
-      app: application ?? '',
-      refresh,
-      page,
+    const { setError, setSuccess } = useErrorContext();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const { tasks, sortField, setSortField, refreshTasksInTimeframe } = useTasks({
+        setError,
+        setSuccess,
     });
-  };
 
-  // Initial load
-  useEffect(() => {
-    refreshTasksInTimeframe(currentTimeframe, currentApplication);
-    updateSearchParameters(currentApplication, currentAutoRefresh, currentPage);
-  }, []);
+    const [currentAutoRefresh, setCurrentAutoRefresh] = useState(
+        searchParams.get('refresh') ?? autoRefreshIntervals['30s'],
+    );
+    const autoRefreshIntervalRef = useRef(null);
+    const [currentApplication, setCurrentApplication] = useState(
+        searchParams.get('app') ?? null,
+    );
+    const currentTimeframe = 9 * 60 * 60;
+    const [currentPage, setCurrentPage] = useState(
+        searchParams.get('page') ? Number(searchParams.get('page')) : 1,
+    );
 
-  // Reset the interval on any state change (because we use the state variables for data retrieval)
-  useEffect(() => {
-    // Reset the current interval
-    if (autoRefreshIntervalRef.current !== null) {
-      clearInterval(autoRefreshIntervalRef.current);
-    }
-
-    if (!currentAutoRefresh) {
-      // Value is 0 for "off"
-      return;
-    }
-
-    // Set interval
-    autoRefreshIntervalRef.current = setInterval(() => {
-      refreshTasksInTimeframe(currentTimeframe, currentApplication);
-    }, currentAutoRefresh * 1000);
-
-    // Clear interval on exit
-    return () => {
-      if (autoRefreshIntervalRef.current !== null) {
-        clearInterval(autoRefreshIntervalRef.current);
-      }
+    const updateSearchParameters = (application, refresh, page) => {
+        setSearchParams({
+            app: application ?? '',
+            refresh,
+            page,
+        });
     };
-  });
 
-  const handleAutoRefreshChange = event => {
-    // Change the value
-    setCurrentAutoRefresh(event.target.value);
-    // Save to URL
-    updateSearchParameters(currentApplication, event.target.value, 1);
-  };
+    // Initial load
+    useEffect(() => {
+        refreshTasksInTimeframe(currentTimeframe, currentApplication);
+        updateSearchParameters(currentApplication, currentAutoRefresh, currentPage);
+    }, []);
 
-  return (
-    <Container maxWidth="xl">
-      <Stack
-        direction={{ xs: 'column', md: 'row' }}
-        spacing={2}
-        alignItems="center"
-        sx={{ mb: 2 }}
-      >
-        <Typography
-          variant="h5"
-          gutterBottom
-          component="div"
-          sx={{
-            flexGrow: 1,
-            display: 'flex',
-            gap: '10px',
-            m: 0,
-            alignItems: 'center', // Center text vertically
-          }}
-        >
-          <Box>Recent tasks</Box>
-          <Box sx={{ fontSize: '10px' }}>UTC</Box>
-        </Typography>
-        <Stack direction="row" spacing={2}>
-          <Box>
-            <ApplicationsFilter
-              value={currentApplication}
-              onChange={value => {
-                setCurrentApplication(value);
-                refreshTasksInTimeframe(currentTimeframe, value);
-                // Reset page
-                setCurrentPage(1);
-                // Update URL
-                updateSearchParameters(value, currentAutoRefresh, 1);
-              }}
-              setError={setError}
-              setSuccess={setSuccess}
-            />
-          </Box>
-          <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth size={'small'}>
-              <InputLabel>Auto-Refresh</InputLabel>
-              <Select
-                value={currentAutoRefresh}
-                label="Auto-Refresh"
-                onChange={handleAutoRefreshChange}
-              >
-                {Object.keys(autoRefreshIntervals).map(autoRefreshInterval => {
-                  let value = autoRefreshIntervals[autoRefreshInterval];
-                  return (
-                    <MenuItem key={autoRefreshInterval} value={value}>
-                      {autoRefreshInterval}
-                    </MenuItem>
-                  );
-                })}
-              </Select>
-            </FormControl>
-          </Box>
-          <Box>
-            <IconButton
-              edge="start"
-              color={'primary'}
-              title={'Force table load'}
-              onClick={() => {
-                // Update tasks
-                refreshTasksInTimeframe(currentTimeframe, currentApplication);
-                // Reset page
-                setCurrentPage(1);
-                updateSearchParameters(
-                  currentApplication,
-                  currentAutoRefresh,
-                  1,
-                );
-              }}
-            >
-              <RefreshIcon />
-            </IconButton>
-          </Box>
-        </Stack>
-      </Stack>
-      {/* Style the table with Material-UI Paper component */}
-      <Box sx={{ boxShadow: 2, borderRadius: 2, p: 2 }}>
-        <TasksTable
-          tasks={tasks}
-          sortField={sortField}
-          setSortField={setSortField}
-          relativeDate={true}
-          page={currentPage}
-          onPageChange={page => {
-            setCurrentPage(page);
-            updateSearchParameters(
-              currentApplication,
-              currentAutoRefresh,
-              page,
-            );
-          }}
-        />
-      </Box>
-    </Container>
-  );
+    // Reset the interval on any state change (because we use the state variables for data retrieval)
+    useEffect(() => {
+        // Reset the current interval
+        if (autoRefreshIntervalRef.current !== null) {
+            clearInterval(autoRefreshIntervalRef.current);
+        }
+
+        if (!currentAutoRefresh) {
+            // Value is 0 for "off"
+            return;
+        }
+
+        // Set interval
+        autoRefreshIntervalRef.current = setInterval(() => {
+            refreshTasksInTimeframe(currentTimeframe, currentApplication);
+        }, currentAutoRefresh * 1000);
+
+        // Clear interval on exit
+        return () => {
+            if (autoRefreshIntervalRef.current !== null) {
+                clearInterval(autoRefreshIntervalRef.current);
+            }
+        };
+    });
+
+    const handleAutoRefreshChange = event => {
+        // Change the value
+        setCurrentAutoRefresh(event.target.value);
+        // Save to URL
+        updateSearchParameters(currentApplication, event.target.value, 1);
+    };
+
+    return (
+        <Container maxWidth="xl">
+            <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: 'calc(100vh - 40px)' }}>
+                <Box sx={{ flexGrow: 1 }}>
+                    <Stack
+                        direction={{ xs: 'column', md: 'row' }}
+                        spacing={2}
+                        alignItems="center"
+                        sx={{ mb: 2 }}
+                    >
+                        <Typography
+                            variant="h5"
+                            gutterBottom
+                            component="div"
+                            sx={{
+                                flexGrow: 1,
+                                display: 'flex',
+                                gap: '10px',
+                                m: 0,
+                                alignItems: 'center', // Center text vertically
+                            }}
+                        >
+                            <Box>Recent tasks</Box>
+                            <Box sx={{ fontSize: '10px' }}>UTC</Box>
+                        </Typography>
+                        <Stack direction="row" spacing={2}>
+                            <Box>
+                                <ApplicationsFilter
+                                    value={currentApplication}
+                                    onChange={value => {
+                                        setCurrentApplication(value);
+                                        refreshTasksInTimeframe(currentTimeframe, value);
+                                        // Reset page
+                                        setCurrentPage(1);
+                                        // Update URL
+                                        updateSearchParameters(value, currentAutoRefresh, 1);
+                                    }}
+                                    setError={setError}
+                                    setSuccess={setSuccess}
+                                />
+                            </Box>
+                            <Box sx={{ minWidth: 120 }}>
+                                <FormControl fullWidth size={'small'}>
+                                    <InputLabel>Auto-Refresh</InputLabel>
+                                    <Select
+                                        value={currentAutoRefresh}
+                                        label="Auto-Refresh"
+                                        onChange={handleAutoRefreshChange}
+                                    >
+                                        {Object.keys(autoRefreshIntervals).map(autoRefreshInterval => {
+                                            let value = autoRefreshIntervals[autoRefreshInterval];
+                                            return (
+                                                <MenuItem key={autoRefreshInterval} value={value}>
+                                                    {autoRefreshInterval}
+                                                </MenuItem>
+                                            );
+                                        })}
+                                    </Select>
+                                </FormControl>
+                            </Box>
+                            <Box>
+                                <IconButton
+                                    edge="start"
+                                    color={'primary'}
+                                    title={'Force table load'}
+                                    onClick={() => {
+                                        // Update tasks
+                                        refreshTasksInTimeframe(currentTimeframe, currentApplication);
+                                        // Reset page
+                                        setCurrentPage(1);
+                                        updateSearchParameters(
+                                            currentApplication,
+                                            currentAutoRefresh,
+                                            1,
+                                        );
+                                    }}
+                                >
+                                    <RefreshIcon />
+                                </IconButton>
+                            </Box>
+                        </Stack>
+                    </Stack>
+                    {/* Style the table with Material-UI Paper component */}
+                    <Box sx={{ boxShadow: 2, borderRadius: 2, p: 2 }}>
+                        <TasksTable
+                            tasks={tasks}
+                            sortField={sortField}
+                            setSortField={setSortField}
+                            relativeDate={true}
+                            page={currentPage}
+                            onPageChange={page => {
+                                setCurrentPage(page);
+                                updateSearchParameters(
+                                    currentApplication,
+                                    currentAutoRefresh,
+                                    page,
+                                );
+                            }}
+                        />
+                    </Box>
+                </Box>
+            </Box>
+            <Box sx={{ position: 'fixed', bottom: 0, width: '100%', textAlign: 'center', padding: '10px' }}>
+                <Typography variant="body2" color="text.secondary">
+                    © {new Date().getFullYear()} Vadim Gedz. This project is licensed under the MIT License.
+                </Typography>
+            </Box>
+        </Container>
+    );
 }
 
 export default RecentTasks;
