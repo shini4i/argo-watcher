e706e55f707ef2651b7c1dd6758fe29fc1038070
Vadim Gedz <vadims@linux-tech.io>
2023-12-05 18:18:25 +0200

    chore: deal with code smells in ApplicationsFilter.js

--- web/src/Components/ApplicationsFilter.js
+++ web/src/Components/ApplicationsFilter.js
@@ -1,6 +1,7 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
-import React, { useEffect, useState } from 'react';
 import { fetchApplications } from '../Services/Data';
 
 function ApplicationsFilter({ value, onChange, setError, setSuccess }) {
@@ -8,30 +9,37 @@ function ApplicationsFilter({ value, onChange, setError, setSuccess }) {
 
   useEffect(() => {
     fetchApplications()
-      .then(items => {
-        setSuccess('fetchApplications', 'Application filter dropdown fetched');
-        setApplications(items);
-      })
-      .catch(error => {
-        setError('fetchApplications', error.message);
-      });
+        .then(items => {
+          setSuccess('fetchApplications', 'Application filter dropdown fetched');
+          setApplications(items);
+        })
+        .catch(error => {
+          setError('fetchApplications', error.message);
+        });
   }, []);
 
   const handleApplicationsChange = (_event, newValue) => {
-    onChange && onChange(newValue);
+    onChange?.(newValue);
   };
 
   return (
-    <Autocomplete
-      size={'small'}
-      disablePortal
-      options={applications}
-      sx={{ width: 220 }}
-      renderInput={params => <TextField {...params} label="Application" />}
-      value={value || null}
-      onChange={handleApplicationsChange}
-    />
+      <Autocomplete
+          size={'small'}
+          disablePortal
+          options={applications}
+          sx={{ width: 220 }}
+          renderInput={params => <TextField {...params} label="Application" />}
+          value={value || null}
+          onChange={handleApplicationsChange}
+      />
   );
 }
+
+ApplicationsFilter.propTypes = {
+  value: PropTypes.any,
+  onChange: PropTypes.func,
+  setError: PropTypes.func.isRequired,
+  setSuccess: PropTypes.func.isRequired
+};
 
 export default ApplicationsFilter;
