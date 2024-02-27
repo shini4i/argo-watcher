4180cc244e90ff940abe9ccbd3079cf9b0e7bb1e
Vadim Gedz <vadims@linux-tech.io>
2024-02-27 16:32:10 +0200

    feat(updater): add support for multi source applications (#254)

---
+++ internal/models/gitops.go
@@ -0,0 +1,46 @@
+package models
+
+import (
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type GitopsRepo struct {
+	RepoUrl    string `validate:"required"`
+	BranchName string `validate:"required"`
+	Path       string `validate:"required"`
+}
+
+func NewGitopsRepo(app *Application) (GitopsRepo, error) {
+	if app.Spec.Sources != nil {
+		return extractGitOverrides(app.Metadata.Annotations)
+	}
+	return GitopsRepo{
+		RepoUrl:    app.Spec.Source.RepoURL,
+		BranchName: app.Spec.Source.TargetRevision,
+		Path:       app.Spec.Source.Path,
+	}, nil
+}
+
+func extractGitOverrides(annotations map[string]string) (GitopsRepo, error) {
+	var gr GitopsRepo
+	fields := map[string]*string{
+		managedGitRepo:   &gr.RepoUrl,
+		managedGitBranch: &gr.BranchName,
+		managedGitPath:   &gr.Path,
+	}
+
+	for key, value := range annotations {
+		if field, ok := fields[key]; ok {
+			*field = value
+		}
+	}
+
+	validate := validator.New()
+	if err := validate.Struct(gr); err != nil {
+		return gr, fmt.Errorf("invalid gitops repo: %w", err)
+	}
+
+	return gr, nil
+}
