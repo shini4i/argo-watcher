d45f1e0b515eec6a147292b516efc5a3b2d95c86
Vadim Gedz <vadims@linux-tech.io>
2024-04-30 23:34:44 +0300

    chore: code cleanup pt.2

--- cmd/argo-watcher/server/server_test.go
+++ cmd/argo-watcher/server/server_test.go
@@ -9,21 +9,10 @@ import (
 
 func TestServer_initLogs_correct(t *testing.T) {
 	// Invoke the function being tested
-	initLogs("fatal", "json")
+	initLogs("fatal")
 
 	// Assert that the global log level is set to the expected value
 	assert.Equal(t, zerolog.FatalLevel, zerolog.GlobalLevel())
-
-	// Cleanup - reset the global log level to the default value
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
-}
-
-func TestServer_initLogs_invalid(t *testing.T) {
-	// Invoke the function being tested
-	initLogs("invalid", "json")
-
-	// Assert that the global log level is set to info level when the log level is invalid
-	assert.Equal(t, zerolog.InfoLevel, zerolog.GlobalLevel())
 
 	// Cleanup - reset the global log level to the default value
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
