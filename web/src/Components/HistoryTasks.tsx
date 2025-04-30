2fa21547cf7002aa45e3fcc5bd6316c48159b5a3
Vadim Gedz <github@shini4i.dev>
2025-04-30 23:44:55 +0300

    feat(web): add support for historical tasks export

--- web/src/Components/HistoryTasks.tsx
+++ web/src/Components/HistoryTasks.tsx
@@ -1,185 +1,248 @@
-import React, { forwardRef, useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import React, {forwardRef, useEffect, useState} from 'react';
+import {useSearchParams} from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
 import RefreshIcon from '@mui/icons-material/Refresh';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import { endOfDay, startOfDay } from 'date-fns';
+import {endOfDay, startOfDay} from 'date-fns';
+import * as XLSX from 'xlsx';
 
 import ApplicationsFilter from './ApplicationsFilter';
-import TasksTable, { useTasks } from './TasksTable';
-import { useErrorContext } from '../ErrorContext';
+import TasksTable, {useTasks} from './TasksTable';
+import {useErrorContext} from '../ErrorContext';
 
-interface DateRangePickerCustomInputProps {
-  value: string;
-  onClick: () => void;
-}
+const modalStyle = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 300,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+    borderRadius: 2,
+};
 
-const DateRangePickerCustomInput = forwardRef<HTMLInputElement, DateRangePickerCustomInputProps>(
-  ({ value, onClick }, ref) => (
-    <TextField
-      size="small"
-      sx={{ minWidth: '220px' }}
-      onClick={onClick}
-      ref={ref}
-      value={value}
-      label="Date range"
-      InputProps={{ readOnly: true }}
-    />
-  )
-);
+const HistoryTasks: React.FC = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const {setError, setSuccess} = useErrorContext();
+    const {tasks, sortField, setSortField, appNames, refreshTasksInRange, clearTasks} =
+        useTasks({setError, setSuccess});
+    const [currentApplication, setCurrentApplication] = useState<string | null>(
+        searchParams.get('app')
+    );
+    const [dateRange, setDateRange] = useState<[Date | null, Date | null]>([
+        searchParams.get('start') ? new Date(Number(searchParams.get('start')) * 1000) : startOfDay(new Date()),
+        searchParams.get('end') ? new Date(Number(searchParams.get('end')) * 1000) : startOfDay(new Date()),
+    ]);
+    const [startDate, endDate] = dateRange;
+    const [currentPage, setCurrentPage] = useState<number>(
+        searchParams.get('page') ? Number(searchParams.get('page')) : 1
+    );
+    const [isExportModalOpen, setExportModalOpen] = useState(false);
 
-DateRangePickerCustomInput.displayName = 'DateRangePickerCustomInput';
+    const updateSearchParameters = (start: Date, end: Date, application: string | null, page: number) => {
+        const params: Record<string, any> = {
+            start: Math.floor(start.getTime() / 1000),
+            end: Math.floor(end.getTime() / 1000),
+        };
 
-interface HistoryTasksProps {}
+        if (application) {
+            params.app = application;
+        }
 
-const HistoryTasks: React.FC<HistoryTasksProps> = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const { setError, setSuccess } = useErrorContext();
-  const { tasks, sortField, setSortField, appNames, refreshTasksInRange, clearTasks } =
-    useTasks({ setError, setSuccess });
-  const [currentApplication, setCurrentApplication] = useState<string | null>(
-    searchParams.get('app')
-  );
-  const [dateRange, setDateRange] = useState<[Date | null, Date | null]>([
-    searchParams.get('start') ? new Date(Number(searchParams.get('start')) * 1000) : startOfDay(new Date()),
-    searchParams.get('end') ? new Date(Number(searchParams.get('end')) * 1000) : startOfDay(new Date()),
-  ]);
-  const [startDate, endDate] = dateRange;
-  const [currentPage, setCurrentPage] = useState<number>(
-    searchParams.get('page') ? Number(searchParams.get('page')) : 1
-  );
+        if (page !== 1) {
+            params.page = page;
+        }
 
-  const updateSearchParameters = (start: Date, end: Date, application: string | null, page: number) => {
-    const params: Record<string, any> = {
-      start: Math.floor(start.getTime() / 1000),
-      end: Math.floor(end.getTime() / 1000),
+        setSearchParams(params);
     };
 
-    if (application) {
-      params.app = application;
-    }
-
-    if (page !== 1) {
-      params.page = page;
-    }
-
-    setSearchParams(params);
-  };
-
-  const refreshWithFilters = (start: Date | null, end: Date | null, application: string | null, page: number) => {
-    if (start && end) {
-      refreshTasksInRange(
-        Math.floor(startOfDay(start).getTime() / 1000),
-        Math.floor(endOfDay(end).getTime() / 1000),
-        application
-      );
-      updateSearchParameters(start, end, application, page);
-    } else {
-      clearTasks();
-    }
-  };
-
-  useEffect(() => {
-    refreshWithFilters(startDate!, endDate!, currentApplication, currentPage);
-  }, [startDate, endDate, currentApplication, currentPage]);
-
-  return (
-    <Container maxWidth="xl">
-      <Stack
-        direction={{ xs: 'column', md: 'row' }}
-        spacing={2}
-        alignItems="center"
-        sx={{ mb: 2 }}
-      >
-        <Typography
-          variant="h5"
-          gutterBottom
-          component="div"
-          sx={{
-            flexGrow: 1,
-            display: 'flex',
-            gap: '10px',
-            m: 0,
-            alignItems: 'center',
-          }}
-        >
-          <Box>History tasks</Box>
-          <Box sx={{ fontSize: '10px' }}>UTC</Box>
-        </Typography>
-        <Stack direction="row" spacing={2}>
-          <Box>
-            <ApplicationsFilter
-              value={currentApplication}
-              onChange={value => {
-                setCurrentApplication(value);
-                setCurrentPage(1);
-                refreshWithFilters(startDate!, endDate!, value, 1);
-              }}
-              appNames={appNames}
-            />
-          </Box>
-          <Box>
-            <DatePicker
-              selectsRange
-              startDate={startDate}
-              endDate={endDate}
-              onChange={(update: [Date | null, Date | null]) => {
-                setDateRange(update);
-                setCurrentPage(1);
-                refreshWithFilters(update[0], update[1], currentApplication, 1);
-              }}
-              maxDate={new Date()}
-              isClearable={false}
-              customInput={
-                <DateRangePickerCustomInput
-                  value={`${startDate ? startDate.toLocaleDateString() : ''} - ${endDate ? endDate.toLocaleDateString() : ''}`}
-                  onClick={() => {}}
-                />
-              }
-              required
-            />
-          </Box>
-          <Box>
-            <IconButton
-              edge="start"
-              color="primary"
-              title="Reload table"
-              onClick={() => {
-                setCurrentPage(1);
-                refreshWithFilters(startDate!, endDate!, currentApplication, 1);
-              }}
-            >
-              <RefreshIcon />
-            </IconButton>
-          </Box>
-        </Stack>
-      </Stack>
-      <Box sx={{ boxShadow: 2, borderRadius: 2, p: 2 }}>
-        <TasksTable
-          tasks={tasks}
-          sortField={sortField}
-          setSortField={setSortField}
-          relativeDate={false}
-          page={currentPage}
-          onPageChange={page => {
-            setCurrentPage(page);
-            updateSearchParameters(
-              startDate!,
-              endDate!,
-              currentApplication,
-              page
+    const refreshWithFilters = (start: Date | null, end: Date | null, application: string | null, page: number) => {
+        if (start && end) {
+            refreshTasksInRange(
+                Math.floor(startOfDay(start).getTime() / 1000),
+                Math.floor(endOfDay(end).getTime() / 1000),
+                application
             );
-          }}
-        />
-      </Box>
-    </Container>
-  );
+            updateSearchParameters(start, end, application, page);
+        } else {
+            clearTasks();
+        }
+    };
+
+    const exportData = (type: 'json' | 'csv' | 'xlsx') => {
+        if (!tasks || tasks.length === 0) {
+            setError('No data available for export.');
+            return;
+        }
+
+        const filename = `tasks_export_${Date.now()}.${type}`;
+        if (type === 'json') {
+            const blob = new Blob([JSON.stringify(tasks, null, 2)], {type: 'application/json'});
+            const link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
+            link.download = filename;
+            link.click();
+        } else if (type === 'csv') {
+            const csvContent = [
+                Object.keys(tasks[0]).join(','), // Header row
+                ...tasks.map(task => Object.values(task).join(',')), // Data rows
+            ].join('\n');
+            const blob = new Blob([csvContent], {type: 'text/csv'});
+            const link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
+            link.download = filename;
+            link.click();
+        } else if (type === 'xlsx') {
+            const worksheet = XLSX.utils.json_to_sheet(tasks);
+            const workbook = XLSX.utils.book_new();
+            XLSX.utils.book_append_sheet(workbook, worksheet, 'Tasks');
+            XLSX.writeFile(workbook, filename);
+        }
+
+        setExportModalOpen(false);
+        setSuccess('Data exported successfully.');
+    };
+
+    useEffect(() => {
+        refreshWithFilters(startDate!, endDate!, currentApplication, currentPage);
+    }, [startDate, endDate, currentApplication, currentPage]);
+
+    return (
+        <Container maxWidth="xl">
+            <Stack
+                direction={{xs: 'column', md: 'row'}}
+                spacing={2}
+                alignItems="center"
+                sx={{mb: 2}}
+            >
+                <Typography
+                    variant="h5"
+                    gutterBottom
+                    component="div"
+                    sx={{
+                        flexGrow: 1,
+                        display: 'flex',
+                        gap: '10px',
+                        m: 0,
+                        alignItems: 'center',
+                    }}
+                >
+                    <Box>History tasks</Box>
+                    <Box sx={{fontSize: '10px'}}>UTC</Box>
+                </Typography>
+                <Stack direction="row" spacing={2}>
+                    <Box>
+                        <ApplicationsFilter
+                            value={currentApplication}
+                            onChange={value => {
+                                setCurrentApplication(value);
+                                setCurrentPage(1);
+                                refreshWithFilters(startDate!, endDate!, value, 1);
+                            }}
+                            appNames={appNames}
+                        />
+                    </Box>
+                    <Box>
+                        <DatePicker
+                            selectsRange
+                            startDate={startDate}
+                            endDate={endDate}
+                            onChange={(update: [Date | null, Date | null]) => {
+                                setDateRange(update);
+                                setCurrentPage(1);
+                                refreshWithFilters(update[0], update[1], currentApplication, 1);
+                            }}
+                            maxDate={new Date()}
+                            isClearable={false}
+                            customInput={
+                                <TextField
+                                    size="small"
+                                    sx={{minWidth: '220px'}}
+                                    value={`${startDate ? startDate.toLocaleDateString() : ''} - ${endDate ? endDate.toLocaleDateString() : ''}`}
+                                    label="Date range"
+                                    InputProps={{readOnly: true}}
+                                />
+                            }
+                            required
+                        />
+                    </Box>
+                    <Box>
+                        <IconButton
+                            edge="start"
+                            color="primary"
+                            title="Reload table"
+                            onClick={() => {
+                                setCurrentPage(1);
+                                refreshWithFilters(startDate!, endDate!, currentApplication, 1);
+                            }}
+                        >
+                            <RefreshIcon/>
+                        </IconButton>
+                    </Box>
+                    <Box>
+                        <Button
+                            variant="contained"
+                            startIcon={<FileDownloadIcon/>}
+                            onClick={() => setExportModalOpen(true)}
+                        >
+                            Export
+                        </Button>
+                    </Box>
+                </Stack>
+            </Stack>
+            <Box sx={{boxShadow: 2, borderRadius: 2, p: 2}}>
+                <TasksTable
+                    tasks={tasks}
+                    sortField={sortField}
+                    setSortField={setSortField}
+                    relativeDate={false}
+                    page={currentPage}
+                    onPageChange={page => {
+                        setCurrentPage(page);
+                        updateSearchParameters(
+                            startDate!,
+                            endDate!,
+                            currentApplication,
+                            page
+                        );
+                    }}
+                />
+            </Box>
+            <Modal
+                open={isExportModalOpen}
+                onClose={() => setExportModalOpen(false)}
+                aria-labelledby="export-modal-title"
+                aria-describedby="export-modal-description"
+            >
+                <Box sx={modalStyle}>
+                    <Typography id="export-modal-title" variant="h6" component="h2">
+                        Select Export Type
+                    </Typography>
+                    <Stack direction="row" spacing={2} sx={{mt: 2}}>
+                        <Button variant="contained" onClick={() => exportData('json')}>
+                            JSON
+                        </Button>
+                        <Button variant="contained" onClick={() => exportData('csv')}>
+                            CSV
+                        </Button>
+                        <Button variant="contained" onClick={() => exportData('xlsx')}>
+                            Excel
+                        </Button>
+                    </Stack>
+                </Box>
+            </Modal>
+        </Container>
+    );
 };
 
 export default HistoryTasks;
