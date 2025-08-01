25fbaa376961a09b48ee55bc67058b64799d9b58
Vadim Gedz <github@shini4i.dev>
2025-08-01 22:21:32 +0300

    chore: Separate client and server into distinct binaries (#378)

--- cmd/argo-watcher/main.go
+++ cmd/argo-watcher/main.go
@@ -1,56 +1,9 @@
 package main
 
 import (
-	"errors"
-	"flag"
-	"fmt"
-	"os"
-
 	"github.com/shini4i/argo-watcher/cmd/argo-watcher/server"
-
-	"github.com/shini4i/argo-watcher/pkg/client"
 )
 
-var errorInvalidMode = errors.New("invalid mode")
-
-func runWatcher(serverFlag, clientFlag bool) error {
-	// start server if requested
-	if serverFlag && !clientFlag {
-		server.RunServer()
-		return nil
-	}
-
-	// start client if requested
-	if clientFlag && !serverFlag {
-		client.Run()
-		return nil
-	}
-
-	// return error. we must start client or server
-	return errorInvalidMode
-}
-
-func printUsage() {
-	if _, err := fmt.Fprintf(os.Stderr, "Usage: argo-watcher [options]\n"); err != nil {
-		return
-	}
-
-	if _, err := fmt.Fprintf(os.Stderr, "Invalid mode specified. Please specify either -server or -client.\n"); err != nil {
-		return
-	}
-
-	flag.PrintDefaults()
-}
-
 func main() {
-	serverFlag := flag.Bool("server", false, "Run in server mode")
-	clientFlag := flag.Bool("client", false, "Run in client mode")
-
-	flag.Usage = printUsage
-	flag.Parse()
-
-	if err := runWatcher(*serverFlag, *clientFlag); err != nil {
-		flag.Usage()
-		os.Exit(1)
-	}
+	server.RunServer()
 }
---
+++ cmd/client/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/shini4i/argo-watcher/pkg/client"
+)
+
+func main() {
+	client.Run()
+}
