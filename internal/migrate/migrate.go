da511b1e791c6e40d5f165c8e0898e48f07e4348
Vadim Gedz <github@shini4i.dev>
2025-08-12 20:27:31 +0300

    fix(migrations): remove redundant import

--- internal/migrate/migrate.go
+++ internal/migrate/migrate.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
