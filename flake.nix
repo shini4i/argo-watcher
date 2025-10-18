e0bbfa51b728d232c26eb6e10455ff1d662376d8
Vadim Gedz <github@shini4i.dev>
2025-10-18 16:13:21 +0300

    update flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,9 @@
 {
   description = "Development environment for argo-watcher with Go tooling and pre-commit hooks";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
@@ -14,7 +12,7 @@
         };
 
         goToolchain = with pkgs; [
-          go
+          go_1_24
           gopls
           gotools
         ];
@@ -28,8 +26,8 @@
         devShells.default = pkgs.mkShell {
           packages = goToolchain ++ preCommitTools;
           shellHook = ''
-            export GOPATH="${PWD}/.go"
-            export GOMODCACHE="${PWD}/.gomod"
+            export GOPATH="${builtins.getEnv "PWD"}/.go"
+            export GOMODCACHE="${builtins.getEnv "PWD"}/.gomod"
             export GO111MODULE=on
           '';
         };
